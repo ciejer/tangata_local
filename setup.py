ff71159b2fa8ee72f0d4a1b4cde015a041bda4af
Chris Jenkins <chris.jenkins@trustpower.co.nz>
2021-06-01 23:05:44 +1200

    Cleanup & visual update for editable fields

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='tangata',
-    url='http://tangata.chrisjenkins.nz',
+    url='https://github.com/ciejer/tangata_local',
     author="Chris Jenkins",
     author_email='chris@chrisjenkins.nz',
-    version='0.0.1',
+    description='An interactive catalog for dbt_',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Database :: Front-Ends",
+        "Framework :: Flask",
+        "Development Status :: 3 - Alpha",
+    ],
+    version='0.1.0',
     packages=find_packages(),
     entry_points={
         'console_scripts': ['tangata=tangata.tangata:tangata',
